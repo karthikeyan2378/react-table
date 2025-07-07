df9753ce27309e9d2cf58fe2d5b18df4a61019ea
ML KARTHIKEYAN LAKSHMI <mlkarthik2378@gmail.com>
2025-07-07 04:46:44 +0000

    i tried to add my component to another project but tailwind styles not l

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,9 @@
-
 import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-    './src/FMComponents/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{ts,tsx,mdx}',
 	],
   prefix: "",
   theme: {
