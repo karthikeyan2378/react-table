dfac87b83043a17eb8077fe4236f84011141bc3d
ML KARTHIKEYAN LAKSHMI <mlkarthik2378@gmail.com>
2025-07-17 17:23:12 +0000

    legend not needed for the chart  when selecting on chart black outline a

--- src/FMComponents/status-chart.tsx
+++ src/FMComponents/status-chart.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 import { type Alarm, alarmConfig } from '../config/alarm-config';
 import './status-chart.css';
 import { useDropdown } from '@/hooks/use-dropdown';
-import { ResponsiveContainer, PieChart, Pie, BarChart, Bar, XAxis, YAxis, Tooltip, Legend, Cell } from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, BarChart, Bar, XAxis, YAxis, Tooltip, Cell } from 'recharts';
 
 /** Reusable Icons as Components **/
 const PieChartIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21.21 15.89A10 10 0 1 1 8 2.83"/><path d="M22 12A10 10 0 0 0 12 2v10z"/></svg>;
@@ -169,8 +169,6 @@ const ColumnChartComponent = ({
                   <Cell 
                     key={`cell-${index}`} 
                     fill={getColor(entry.name, index)} 
-                    stroke={activeFilters.includes(entry.name) ? '#333' : 'none'}
-                    strokeWidth={2}
                   />
                 ))}
               </Bar>
@@ -193,12 +191,9 @@ const ColumnChartComponent = ({
                   <Cell 
                     key={`cell-${index}`} 
                     fill={getColor(entry.name, index)} 
-                    stroke={activeFilters.includes(entry.name) ? '#333' : 'none'}
-                    strokeWidth={2}
                   />
                 ))}
               </Pie>
-              <Legend iconSize={10} wrapperStyle={{fontSize: '11px', bottom: -5}} />
             </PieChart>
           )}
         </ResponsiveContainer>
